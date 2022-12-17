01852ac1577fa41f7b5867d3f9712306ca64ba64
dedani4 <dedani4@gmail.com>
2022-12-17 21:43:47 +0300

    add Example 005 if-else

---
+++ Example005_ifElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter your name: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "masha"){
+    Console.WriteLine("Hi Masha, welcome back!");
+}
+else{
+Console.Write("Hello, ");
+Console.Write(username);
+}
+
+
