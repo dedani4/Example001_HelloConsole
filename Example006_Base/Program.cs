c147e0bb211986c6530d5975f6ec5608d00b664c
dedani4 <dedani4@gmail.com>
2022-12-17 22:11:34 +0300

    add Example006 max finding

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 1, b = 2, c = 6, d = 9, e = 3;
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.Write(max);
+
+
