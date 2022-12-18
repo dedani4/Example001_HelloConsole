170958bf0821e8b702ab1ca8f52ca70870a9e448
Anton <dedani4@gmail.com>
2022-12-18 18:09:06 +0300

    Add Example010 Method & Array

---
+++ Example010_Method&Array/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = {5,7,4,9,89,45,2,76,14,4,43};
+
+ int find = 4;
+ int n = array.Length;
+
+ int index = 0;
+
+ while (index < n){
+    if (array[index] == find) {
+        Console.WriteLine (index);
+        break;
+    }
+    index++;
+ }
