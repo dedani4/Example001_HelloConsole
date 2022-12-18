93996e9e8caa296953c73fc485dbfafce13b9e4b
dedani4 <dedani4@gmail.com>
2022-12-18 11:36:53 +0300

    add Example008 Friends & Dog

---
+++ Example008_Friends&Dog/Program.cs
@@ -0,0 +1,19 @@
+﻿int count = 0, distance = 100, 
+    speedFirstFriend = 1, speedSecondFriend = 2,
+    speedDog = 5,
+    friendDirection = 2;
+
+while (distance>10){
+    int time = 0;
+    if (friendDirection == 1){
+        time = distance / (speedFirstFriend + speedDog);
+        friendDirection = 2;
+    } else {
+        time = distance / (speedSecondFriend + speedDog);
+        friendDirection = 2;
+    }
+    distance = distance -(speedFirstFriend + speedSecondFriend) * time;
+    count++;
+}
+Console.WriteLine(count);
+
